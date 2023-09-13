2ba9760b36ca32ff462710650bb9ddf8e22c11ee
avidson William <davidsonwferreira8@gmail.com>
2023-09-13 17:54:28 -0300

    pronto

--- script.js
+++ script.js
@@ -12,8 +12,7 @@ function iniciar() {
         alert("Nome invalido estamos esperando um nome com mais de 3 caracteres");
         nome = prompt("Digite o nome novamente."); 
     }
-
-    document.getElementById("nome").innerHTML = nome;
+    let text = document.getElementById('nome').value;
 }
 
 
